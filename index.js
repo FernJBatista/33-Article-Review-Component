eb81f27bc2c2094eed446b80ab6aa4ef8cffe192
Fernando Batista <fernjbatista@gmail.com>
2025-02-18 03:36:52 +0100

    Update

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 addEventListener("DOMContentLoaded", function() {
     const shareContainer = document.querySelector("#shareContainer");
-    const shareIcon = document.querySelector("#shareIcon");
-    shareIcon.addEventListener("click", function() {
+    const shareButton = document.querySelector("#shareButton");
+    shareButton.addEventListener("click", function() {
         shareContainer.classList.toggle("active");
-        
     });
 });
